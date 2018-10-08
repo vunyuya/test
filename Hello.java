0204aa32b6b6e2d219a5e1e65262576645905076
Ежов Иван Владиславович <Ezhov@WUD.lan>
2018-10-08 13:13:59 +0300

    Change in feature-2

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
 	System.out.println("A");	
-        System.out.println("Hello World!");
+        System.out.println("Hello World!2");
 	System.out.println("B");
     }
 }
